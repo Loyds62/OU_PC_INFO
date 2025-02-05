c5b20af32ce71f81dd1c7c9fcc2ae02af99591ac
Loyds62 <renaudroussel@icloud.com>
2025-02-05 14:54:41 +0100

    Add files via upload

---
+++ OU_PC.ps1
@@ -0,0 +1,14 @@
+﻿# Demande le nom de l'ordinateur à l'utilisateur
+$computerName = Read-Host "Entrez le nom de l'ordinateur"
+
+# Recherche l'ordinateur dans Active Directory
+$computer = Get-ADComputer -Identity $computerName
+
+# Vérifie si l'ordinateur a été trouvé
+if ($computer) {
+    # Affiche l'OU de l'ordinateur
+    $ou = $computer.DistinguishedName -replace '^CN=.*?,', ''
+    Write-Output "L'ordinateur $computerName se trouve dans l'OU : $ou"
+} else {
+    Write-Output "L'ordinateur $computerName n'a pas été trouvé dans Active Directory."
+}
